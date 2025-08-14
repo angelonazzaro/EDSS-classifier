6b0934bee50fca77e342a7346464cae9ee3a5a5a
Angelo Nazzaro <58223071+angelonazzaro@users.noreply.github.com>
2025-08-14 11:58:56 +0200

    Init git lfs

--- edss_dataset/edss_dataset.py
+++ edss_dataset/edss_dataset.py
@@ -12,20 +12,23 @@ from utils.constants import CLASS_THRESHOLDS
 logging.basicConfig(level=logging.INFO)
 
 
-def preprocess_image(image: Union[np.ndarray, EagerTensor], label: int,
-                     normalize: bool = True):
+def preprocess_image(
+    image: Union[np.ndarray, EagerTensor], label: int, normalize: bool = True
+):
     """
-        Preprocess image by applying rescaling, normalization and augmentation.
+    Preprocess image by applying rescaling, normalization and augmentation.
 
-        Args:
-            - image: (np.ndarray, EagerTensor): the image to preprocess (expects grayscale image)
-            - label: (int): the label of the image
-            - normalize: (bool): whether to normalize the image
-            - transformations: (Optional[List[Callable]]): the transformations to apply. If None,
-                a set of default transformations will be applied.
+    Args:
+        - image: (np.ndarray, EagerTensor): the image to preprocess (expects grayscale image)
+        - label: (int): the label of the image
+        - normalize: (bool): whether to normalize the image
+        - transformations: (Optional[List[Callable]]): the transformations to apply. If None,
+            a set of default transformations will be applied.
     """
     if image.ndim != 2 and (image.ndim == 3 and image.shape[-1] != 1):
-        raise ValueError('Image must be grayscale. Expected shapes: (H, W) or (H, W, 1).')
+        raise ValueError(
+            "Image must be grayscale. Expected shapes: (H, W) or (H, W, 1)."
+        )
 
     if normalize:
         if isinstance(image, np.ndarray):
@@ -37,27 +40,31 @@ def preprocess_image(image: Union[np.ndarray, EagerTensor], label: int,
     return image, label
 
 
-def get_dataset(data_dir: str,
-                modality: Union[Literal["T1", "T2", "FLAIR"], List[Literal["T1", "T2", "FLAIR"]]] = "T1",
-                split: Literal["train", "val", "test"] = "train",
-                task: Literal["binary", "multi-class"] = "binary",
-                batch_size: int = 32,
-                resize: Optional[Tuple[int, int]] = (256, 256),
-                normalize: bool = True,
-                include_augmented: bool = False) -> tf.data.Dataset:
+def get_dataset(
+    data_dir: str,
+    modality: Union[
+        Literal["T1", "T2", "FLAIR"], List[Literal["T1", "T2", "FLAIR"]]
+    ] = "T1",
+    split: Literal["train", "val", "test"] = "train",
+    task: Literal["binary", "multi-class"] = "binary",
+    batch_size: int = 32,
+    resize: Optional[Tuple[int, int]] = (256, 256),
+    normalize: bool = True,
+    include_augmented: bool = False,
+) -> tf.data.Dataset:
     """
-        Load and preprocess the dataset.
-        The data will be loaded from `data_dir/modality/task/split`.
-        Only `.png` files will be considered. Files must have the label specified in their filepath as '_label_.png'
+    Load and preprocess the dataset.
+    The data will be loaded from `data_dir/modality/task/split`.
+    Only `.png` files will be considered. Files must have the label specified in their filepath as '_label_.png'
 
-        Args:
-            - data_dir: directory to load the dataset from.
-            - split: one of "train", "val", "test". Defaults to "train".
-            - task: one of "binary", "multi-class". Defaults to "binary".
-            - batch_size: batch size to use for training and preprocessing. Defaults to 32.
-            - resize: (Tuple[int, int]): the rescaling factor
-            - normalize: (bool): whether to normalize the image
-            - include_augmented: (bool): whether to include augmented images
+    Args:
+        - data_dir: directory to load the dataset from.
+        - split: one of "train", "val", "test". Defaults to "train".
+        - task: one of "binary", "multi-class". Defaults to "binary".
+        - batch_size: batch size to use for training and preprocessing. Defaults to 32.
+        - resize: (Tuple[int, int]): the rescaling factor
+        - normalize: (bool): whether to normalize the image
+        - include_augmented: (bool): whether to include augmented images
 
     """
     if isinstance(modality, str):
@@ -92,7 +99,9 @@ def get_dataset(data_dir: str,
             try:
                 label = task_labels.index(label)
             except ValueError:
-                logging.warning(f"Label '{label}' not found in CLASS_THRESHOLDS for task '{task}'. Skipping file.")
+                logging.warning(
+                    f"Label '{label}' not found in CLASS_THRESHOLDS for task '{task}'. Skipping file."
+                )
                 continue
 
             images.append(image)
@@ -106,17 +115,20 @@ def get_dataset(data_dir: str,
 
     dataset = dataset.batch(batch_size).map(
         lambda img, lbl: preprocess_image(img, lbl, normalize=normalize),
-        num_parallel_calls=tf.data.AUTOTUNE
+        num_parallel_calls=tf.data.AUTOTUNE,
     )
 
     if split == "train":
         dataset = dataset.shuffle(buffer_size=len(dataset))
 
-    logging.info(f"Dataset loaded from {dataset_dir}\n"
-                 "\t- Modality: {modality}\n"
-                 f"\t- Task: {task}\n"
-                 f"\t- Split: {split}\n"
-                 f"\t- Number of samples: {len(images)}\n"
-                 f"\t- Batch size: {batch_size}\n")
+    logging.info(
+        f"Dataset loaded from {dataset_dir}\n"
+        "\t- Modality: {mod}\n"
+        f"\t- Task: {task}\n"
+        f"\t- Split: {split}\n"
+        f"\t- Number of samples: {len(images)}\n"
+        f"\t- Batch size: {batch_size}\n"
+    )
 
     return dataset
+
